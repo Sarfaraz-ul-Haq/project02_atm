593bd89c5431302fa7673035c223df6352041bef
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-02-06 18:38:13 +0500

    cash withdrawal only

---
+++ app.ts
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import showBanner from "node-banner";
+
+// function to display atm title and tagline
+const displayTitleAndTagline = async () => {
+  await showBanner(
+    `     CLI ATM`,
+    `  Welcome to this console-based ATM application
+   _____________________________________________
+  \n`,
+    "green",
+    "gray"
+  );
+};
+
+// function to prompt user to enter user ID and pin
+const promptUserIdAndPin = async () => {
+  let { userID, userPin } = await inquirer.prompt([
+    {
+      name: "ID",
+      type: "input",
+      message: chalk.gray("Please enter your user ID: "),
+    },
+    {
+      name: "pin",
+      type: "password",
+      message: chalk.gray("Please enter your user pin: "),
+    },
+  ]);
+};
+let bankBalance = Math.floor(Math.random() * 1000000 + 1);
+
+// function to prompt user to select an option
+const promptUserToSelectOption = async () => {
+  let selectedOption = await inquirer.prompt([
+    {
+      name: "option",
+      type: "list",
+      choices: ["Cash withdraw", "Exit"],
+      message: chalk.gray(`\n Select any option`),
+    },
+  ]);
+
+  if (selectedOption.option == "Cash withdraw") {
+    console.log(
+      chalk.green(`
+  Current Bank Balance: ${bankBalance}
+  ----------------------------`)
+    );
+    const withdrawAmount = await inquirer.prompt([
+      {
+        name: "amount",
+        type: "number",
+        message: chalk.gray(`\n Please enter your amount: `),
+      },
+    ]);
+    bankBalance -= withdrawAmount.amount;
+    console.log(
+      chalk.green(`\n (Bank Balance after Withdrawal: ${bankBalance})`)
+    );
+  }
+};
+await displayTitleAndTagline();
+await promptUserIdAndPin();
+promptUserToSelectOption();
